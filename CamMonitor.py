7af7c7724295cb2027f093e6c3e783af3a87b55c
Ebenezer Scrooge <1641367382@qq.com>
2020-07-25 10:24:31 +0800

    Update CamMonitor.py

--- CamMonitor.py
+++ CamMonitor.py
@@ -338,7 +338,7 @@ def delete_file(dir_path, i):
 def start_server():
     MyRequestHandler.process = process_dir
     server = HTTPServer(("", 5673), MyRequestHandler)
-    print("pythonic-simple-http-server started, serving at http://localhost:5673/imr-monitor-server/parsevideo")
+    print("Serving at http://localhost:5673/imr-monitor-server/parsevideo")
     server.serve_forever()
 
 
