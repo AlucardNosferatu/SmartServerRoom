78e950f5f5ce74552dbeb440b38def52c3ef3ba0
Ebenezer Scrooge <1641367382@qq.com>
2020-07-30 11:03:00 +0800

    Backup

--- Obsolete/TempMatch.py
+++ Obsolete/TempMatch.py
@@ -53,39 +53,52 @@ def rotate_bound(image, angle):
     return cv2.warpAffine(image, M, (nW, nH))
 
 
-def NaiveMatcher():
+def naive_matcher():
     # img = cv2.imread('Samples\\LMS\\6.PNG', 0)
-    template = cv2.imread('Samples\\temp2.jpg', 1)
-    # while True:
-    #     for i in range(360):
-    #         rot = rotate_bound(template, i)
-    #         rot = change_size(rot)
-    path = "C:\\Users\\16413\\Downloads\\pic(1)"
+    template = cv2.imread('..\\Samples\\temp2.jpg', 1)
+    path = "..\\Samples\\LMS"
+    # orb = cv2.ORB_create()
+    surf = cv2.xfeatures2d.SURF_create()
     for e, file_name in enumerate(os.listdir(path)):
         if file_name.endswith(".PNG"):
             img = cv2.imread(os.path.join(path, file_name))
             rot = template
-            cv2.imshow("rot", rot)
-            # orb = cv2.ORB_create()
-            orb = cv2.ORB_create()
-            kp1, des1 = orb.detectAndCompute(rot, None)
-            kp2, des2 = orb.detectAndCompute(img, None)
-            # bf = cv2.BFMatcher(normType=cv2.NORM_HAMMING, crossCheck=True)
-            bf = cv2.BFMatcher(normType=cv2.NORM_L1, crossCheck=True)
-            matches = bf.match(des1, des2)
-            matches = sorted(matches, key=lambda x: x.distance)
-            print(len(matches))
-            img2 = cv2.drawMatches(img1=rot, keypoints1=kp1, img2=img, keypoints2=kp2, matches1to2=matches, outImg=img,
-                                   flags=2)
-            # region Match template
-            # h, w = rot.shape[:2]
-            # res = cv2.matchTemplate(img, rot, cv2.TM_SQDIFF_NORMED)
-            # min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-            # left_top = max_loc  # 左上角
-            # right_bottom = (left_top[0] + w, left_top[1] + h)  # 右下角
-            # img_copy = img.copy()
-            # cv2.rectangle(img_copy, left_top, right_bottom, 255, 2)  # 画出矩形位置
-            # cv2.imshow("img", img_copy)
-            # endregion
-            cv2.imshow("res", img2)
-            cv2.waitKey()
+            for i in range(360):
+                print(i)
+                rot = rotate_bound(template, i)
+                rot = change_size(rot)
+                cv2.imshow("rot", rot)
+                # kp1, des1 = orb.detectAndCompute(rot, None)
+                # kp2, des2 = orb.detectAndCompute(img, None)
+                kps1, des_s1 = surf.detectAndCompute(rot, None)
+                kps2, des_s2 = surf.detectAndCompute(img, None)
+                # bf = cv2.BFMatcher(normType=cv2.NORM_L2, crossCheck=True)
+                fm = cv2.FlannBasedMatcher()
+                matches = fm.match(des_s1, des_s2)
+                matches = sorted(matches, key=lambda x: x.distance)
+                # print(len(matches))
+                img2 = cv2.drawMatches(
+                    img1=rot,
+                    keypoints1=kps1,
+                    img2=img,
+                    keypoints2=kps2,
+                    matches1to2=matches,
+                    outImg=img,
+                    flags=2
+                )
+                # region Match template
+                # h, w = rot.shape[:2]
+                # res = cv2.matchTemplate(img, rot, cv2.TM_SQDIFF_NORMED)
+                # min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+                # left_top = max_loc  # 左上角
+                # right_bottom = (left_top[0] + w, left_top[1] + h)  # 右下角
+                # img_copy = img.copy()
+                # cv2.rectangle(img_copy, left_top, right_bottom, 255, 2)  # 画出矩形位置
+                # cv2.imshow("img", img_copy)
+                # endregion
+                cv2.imshow("res", img2)
+                cv2.waitKey(1)
+
+
+if __name__ == '__main__':
+    naive_matcher()
--- QRCode.py
+++ QRCode.py
@@ -1,25 +1,28 @@
 import os
 
-import cv2
 import zxing
 from tqdm import tqdm
 
 
 def batch_filter():
-    pass_list = []
-    path = 'C:/BaiduNetdiskDownload/202005/202005.v2'
+    # path = 'C:/BaiduNetdiskDownload/202005/202005.v2'
+    path = '/tmp/Photos'
     zx = zxing.BarCodeReader()
     img_list = os.listdir(path)
-    for file_name in tqdm(img_list):
-        file_path = os.path.join(path, file_name)
-        zx_data = zx.decode(file_path)
-        if len(zx_data.raw) > 0:
-            # img_array = cv2.imread(file_path)
-            # cv2.imshow('detected',img_array)
-            # cv2.waitKey()
-            pass_list.append(file_name + '\n')
-    with open('valid_file.txt', mode='w', encoding='utf-8') as f:
-        f.writelines(pass_list)
+    img_list.sort()
+    with open('valid_file.txt', mode='a+', encoding='utf-8') as f:
+        f.seek(0)
+        checked_length = len(f.readlines())
+        f.seek(checked_length)
+        for file_name in tqdm(img_list[checked_length:]):
+            file_path = os.path.join(path, file_name)
+            zx_data = zx.decode(file_path)
+            if zx_data is not None and len(zx_data.raw) > 0:
+                f.write(file_name + '\t' + '1' + '\n')
+                f.flush()
+            else:
+                f.write(file_name + '\t' + '0' + '\n')
+                f.flush()
 
     print('Done')
 
