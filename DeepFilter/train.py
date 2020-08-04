683be63b022df5a555e25e982476d5dec7ce36df
Ebenezer Scrooge <1641367382@qq.com>
2020-08-04 09:13:45 +0800

    Update train.py

--- DeepFilter/train.py
+++ DeepFilter/train.py
@@ -1,12 +1,13 @@
 import os
-
 import cv2
+import random
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import Model
-from tensorflow.keras.applications import VGG16
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.layers import Input, Dense, Flatten
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 
 batch_size = 32
 img_dir_path = 'C:/BaiduNetdiskDownload/202005/202005.v2'
@@ -23,26 +24,37 @@ def build_model():
 
 
 def data_generator():
+    labels = []
+    images = []
+    with open('../valid_file.txt', mode='r', encoding='utf-8') as f:
+        lines = f.readlines()
     while True:
-        with open('../valid_file.txt', mode='r', encoding='utf-8') as f:
+        while len(images) < batch_size:
+            line = random.choice(lines)
+            img_name, label = line.strip().split('\t')
+            labels.append(int(label))
+            img_path = os.path.join(img_dir_path, img_name)
+            image = cv2.imread(img_path)
+            image = cv2.resize(image, (224, 224))
+            image = preprocess_input(image)
+            images.append(image)
+        if len(images) >= batch_size:
+            x = np.array(images[:batch_size])
+            y = np.array(labels[:batch_size])
+            y = to_categorical(y, num_classes=2)
+            # print('Yield now')
+            yield x, y
             labels = []
             images = []
-            for i in range(batch_size):
-                line = f.readline()
-                if line:
-                    img_name, label = line.strip().split('\t')
-                    labels.append(labels)
-                    img_path = os.path.join(img_dir_path, img_name)
-                    image = cv2.imread(img_path)
-                    image = cv2.resize(image, (224, 224))
-                    images.append(image)
-            if len(images) == batch_size:
-                x = np.array(images)
-                y = np.array(labels)
-                y = to_categorical(y, num_classes=2)
-                yield x, y
 
 
 def train_model():
     model = build_model()
-    model.fit_generator(generator=data_generator(), epochs=100)
+
+    model.compile(loss=tf.keras.losses.categorical_crossentropy)
+    model.fit_generator(generator=data_generator(), steps_per_epoch=100, epochs=100)
+
+
+if __name__ == '__main__':
+    # data_generator()
+    train_model()
