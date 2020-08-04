5c077d04393bc6c5ad62615b555ddbf3f3e123ba
Ebenezer Scrooge <1641367382@qq.com>
2020-08-04 19:50:36 +0800

    Update train.py

--- DeepFilter/train.py
+++ DeepFilter/train.py
@@ -1,11 +1,11 @@
 import os
-import cv2
 import random
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import Model
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.layers import Input, Dense, Flatten
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 
@@ -38,10 +38,12 @@ def data_generator(mode='train'):
         while len(images) < batch_size:
             line = random.choice(lines)
             img_name, label = line.strip().split('\t')
+            if labels.count(0) >= (batch_size / 2) and label == '0':
+                continue
             labels.append(int(label))
             img_path = os.path.join(img_dir_path, img_name)
-            image = cv2.imread(img_path)
-            image = cv2.resize(image, (224, 224))
+            image = load_img(path=img_path, target_size=(224, 224))
+            image = img_to_array(img=image)
             image = preprocess_input(image)
             images.append(image)
         if len(images) >= batch_size:
@@ -58,7 +60,7 @@ def train_model():
     model = build_model()
     cp_checkpoint = ModelCheckpoint(
         filepath='../Models/QRCode_Detector.h5',
-        monitor='val_loss',
+        monitor='loss',
         verbose=1,
         save_best_only=True,
         save_freq='epoch',
@@ -66,7 +68,7 @@ def train_model():
     )
     print("cp_checkpoint added.")
     es_checkpoint = EarlyStopping(
-        monitor='val_loss',
+        monitor='loss',
         min_delta=0,
         patience=5,
         verbose=1,
@@ -82,7 +84,8 @@ def train_model():
     print("tb_checkpoint added.")
     model.compile(
         loss=tf.keras.losses.categorical_crossentropy,
-        optimizer=tf.keras.optimizers.Adam(lr=0.001)
+        optimizer=tf.keras.optimizers.Adam(lr=0.0001),
+        metrics=['acc']
     )
     if os.path.exists('../Models/QRCode_Detector.h5'):
         model.load_weights(filepath='../Models/QRCode_Detector.h5')
@@ -90,7 +93,7 @@ def train_model():
         model.fit_generator(
             generator=data_generator(mode='train'),
             validation_data=data_generator(mode='test'),
-            steps_per_epoch=100,
+            steps_per_epoch=1000,
             validation_steps=2,
             epochs=100,
             callbacks=[
