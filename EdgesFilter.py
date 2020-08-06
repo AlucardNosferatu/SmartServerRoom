20d1ffdbb08d980e5377457fdde5a53a3314c0d8
Ebenezer Scrooge <1641367382@qq.com>
2020-08-06 15:36:32 +0800

    Update EdgesFilter.py

--- EdgesFilter.py
+++ EdgesFilter.py
@@ -17,7 +17,7 @@ def length_filter(lines, img_h, img_w, mode='h'):
         dx = abs(x2 - x1)
         dy = abs(y2 - y1)
         distance = sqrt((dx ** 2) + (dy ** 2))
-        if 0.9 * th > distance > th / 5:
+        if 0.99 * th > distance > th / 3:
             new_lines.append(line)
     return new_lines
 
@@ -38,7 +38,7 @@ def interval_aggregation(y):
     return count, intervals
 
 
-def combine_short_lines(lines, mode='h'):
+def csl_via_centers(lines, mode='h'):
     xc_list = []
     yc_list = []
     for line in lines:
@@ -58,6 +58,8 @@ def combine_short_lines(lines, mode='h'):
     for i in range(len(count)):
         xy1_list = []
         xy2_list = []
+        yx1_list = []
+        yx2_list = []
         if len(count[i]) >= 1:
             for index in count[i]:
                 x1, y1, x2, y2 = lines[index][0]
@@ -66,24 +68,39 @@ def combine_short_lines(lines, mode='h'):
                     x1 -= x2
                     x2 += x1
                     x1 = (-x1)
+                if y1 > y2:
+                    y2 += y1
+                    y1 -= y2
+                    y2 += y1
+                    y1 = (-y1)
                 if mode == "h":
                     xy1_list.append(x1)
                     xy2_list.append(x2)
+                    yx1_list.append(y1)
+                    yx2_list.append(y2)
                 elif mode == "v":
                     xy1_list.append(y1)
                     xy2_list.append(y2)
+                    yx1_list.append(x1)
+                    yx2_list.append(x2)
                 else:
                     raise ValueError("mode must be 'h' or 'v'.")
             min_xy1 = min(xy1_list)
             max_xy2 = max(xy2_list)
+            min_yx1 = yx1_list[xy1_list.index(min_xy1)]
+            max_yx2 = yx2_list[xy2_list.index(max_xy2)]
             if mode == "h":
                 x1 = min_xy1
                 x2 = max_xy2
+                y1 = min_yx1
+                y2 = max_yx2
                 # y1 = int((intervals[i] + intervals[i + 1]) / 2)
                 # y2 = y1
             elif mode == "v":
                 y1 = min_xy1
                 y2 = max_xy2
+                x1 = min_yx1
+                x2 = max_yx2
                 # x1 = int((intervals[i] + intervals[i + 1]) / 2)
                 # x2 = y1
             else:
@@ -93,13 +110,38 @@ def combine_short_lines(lines, mode='h'):
     return output_lines
 
 
+def csl_with_slope(lines, mode='h'):
+
+    pass
+
+
 if __name__ == "__main__":
-    img = cv2.imread("Samples/LCD.jpg")
-    u, d, l, r = get_4edges(img)
-    d = combine_short_lines(lines=d, mode='h')
-    # d = length_filter(lines=d, img_h=img.shape[0], img_w=img.shape[1], mode='h')
-    for line in d:
+    img = cv2.imread("Samples/HMI2.jpg")
+    up, down, left, right = get_4edges(img)
+
+    up = csl_via_centers(lines=up, mode='h')
+    # up = length_filter(lines=up, img_h=img.shape[0], img_w=img.shape[1], mode='h')
+
+    down = csl_via_centers(lines=down, mode='h')
+    down = length_filter(lines=down, img_h=img.shape[0], img_w=img.shape[1], mode='h')
+
+    left = csl_via_centers(lines=left, mode='v')
+    left = length_filter(lines=left, img_h=img.shape[0], img_w=img.shape[1], mode='v')
+
+    right = csl_via_centers(lines=right, mode='v')
+    right = length_filter(lines=right, img_h=img.shape[0], img_w=img.shape[1], mode='v')
+
+    for line in up:
         x1, y1, x2, y2 = line[0]
         img = cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    for line in down:
+        x1, y1, x2, y2 = line[0]
+        img = cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    for line in left:
+        x1, y1, x2, y2 = line[0]
+        img = cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+    for line in right:
+        x1, y1, x2, y2 = line[0]
+        img = cv2.line(img, (x1, y1), (x2, y2), (255, 255, 0), 2)
     cv2.imshow("line_detect_possible_demo", img)
     cv2.waitKey()
