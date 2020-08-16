5193dfbfcc1288d9f1fb942e1f79bd282f05e63a
Ebenezer Scrooge <1641367382@qq.com>
2020-08-16 12:18:12 +0800

    Add Flask

--- UseDlib.py
+++ UseDlib.py
@@ -1,7 +1,8 @@
 import os
+
+import cv2
 import dlib
 import numpy
-import cv2
 
 predictor_path = 'Models/shape_predictor_68_face_landmarks.dat'
 face_rc_model_path = 'Models/dlib_face_recognition_resnet_model_v1.dat'
@@ -98,28 +99,61 @@ def test_single_image():
         print(result)
 
 
-def test_cam():
-    detector, feature_point, feature_model = init_detectors()
-    descriptors, name_list = get_recorded_features()
-    '''
-    对单张人脸进行识别
-    '''
-    # sample = cv2.VideoCapture(0 +
-    file_path = "Outputs/010tMonitorCollect202008030345203367831f74d7_281_0.mp4"
+def test_cam(detectors=None, file_path="Samples/00010001689000000.mp4", file_name='00010001689000000.mp4'):
+    if detectors is None:
+        detector, feature_point, feature_model = init_detectors()
+    else:
+        detector, feature_point, feature_mode = detectors
+    # descriptors, name_list = get_recorded_features()
     sample = cv2.VideoCapture(file_path)
+    # sample.set(cv2.CAP_PROP_POS_FRAMES, 7000)
+    f_count = -1
     while sample.isOpened():
+        # k = cv2.waitKey(50)
+        print(sample.get(cv2.CAP_PROP_POS_FRAMES))
         ret, test_img = sample.read()
         if test_img is not None:
-            dets = detector(test_img, 1)
-            test_feature = None
-            cv2.imshow('detected face:', test_img)
-            for k, d in enumerate(dets):
+            # cv2.imshow('cam:', test_img)
+            # if k & 0xff == ord('q'):
+            #     break
+            # elif k & 0xff == ord('e'):
+            #     sample.grab()
+            #     continue
+            detected = detector(test_img, 1)
+            for k, d in enumerate(detected):
+                x1 = d.left()
+                x2 = d.right()
+                y1 = d.top()
+                y2 = d.bottom()
+                w = x2 - x1
+                h = y2 - y1
+                dw = int(0.25 * w)
+                dh = int(0.25 * h)
+                x1 -= dw
+                if x1 < 0:
+                    x1 = 0
+                x2 += dw
+                if x2 > test_img.shape[1] - 1:
+                    x2 = test_img.shape[1] - 1
+                y1 -= dh
+                if y1 < 0:
+                    y1 = 0
+                y2 += dh
+                if y2 > test_img.shape[0] - 1:
+                    y2 = test_img.shape[0] - 1
+
+                # cv2.imshow('detected face:', test_img[y1:y2, x1:x2, :])
+                f_count += 1
+                cv2.imwrite('Outputs/' + file_name.split('.')[0] + '_' + str(f_count) + '.jpg',
+                            test_img[y1:y2, x1:x2, :])
                 shape = feature_point(test_img, d)
                 key_points = list(shape.parts())
                 test_img_copy = test_img.copy()
                 for point in key_points:
                     test_img_copy = cv2.circle(test_img_copy, (point.x, point.y), 1, (255, 0, 0), 4)
-                cv2.imshow('detected face:', test_img_copy)
+                # cv2.imshow('cam:', test_img_copy)
+                cv2.imwrite('Outputs/' + file_name.split('.')[0] + '_' + str(f_count) + '_p.jpg',
+                            test_img_copy[y1:y2, x1:x2, :])
                 # test_feature = feature_model.compute_face_descriptor(test_img, shape)
                 # test_feature = numpy.array(test_feature)
                 # dist = []
@@ -134,12 +168,18 @@ def test_cam():
                 # # 截取姓名字符串，去掉末尾的.jpg
                 # result = name_list[min_dist][:-4]
                 # print(result)
-        k = cv2.waitKey(50)
-        if k & 0xff == ord('q'):
-            break
     sample.release()
     cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    test_cam()
+    print("Start loading")
+    detector, feature_point, feature_model = init_detectors()
+    detectors = [detector, feature_point, feature_model]
+    print("Model loaded")
+    dir_path = '../Faces'
+    file_list = os.listdir(dir_path)
+    for i in file_list:
+        if i.endswith('.mp4'):
+            file_path = os.path.join(dir_path, i)
+            test_cam(detectors=detectors, file_path=file_path, file_name=i)
