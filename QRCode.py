841142a5732fc14fe88cee9e591076539c01d2ca
Ebenezer Scrooge <1641367382@qq.com>
2020-08-20 13:25:16 +0800

    Fuck it

--- QRCode.py
+++ QRCode.py
@@ -1,11 +1,13 @@
 import os
 
+import cv2
 import zxing
+import numpy as np
 from tqdm import tqdm
 
 
 def batch_filter():
-    path = 'C:/BaiduNetdiskDownload/202005/202005.v2'
+    path = 'D:/BaiduNetdiskDownload/202005/202005.v2'
     # path = '/tmp/Photos'
     zx = zxing.BarCodeReader()
     img_list = os.listdir(path)
@@ -18,8 +20,9 @@ def batch_filter():
             file_path = os.path.join(path, file_name)
             zx_data = zx.decode(file_path)
             if zx_data is not None and len(zx_data.raw) > 0:
-                f.write(file_name + '\t' + '1' + '\n')
-                f.flush()
+                if 'http://xfujian.189.cn' in zx_data.raw:
+                    f.write(file_name + '\t' + '1' + '\n')
+                    f.flush()
             else:
                 f.write(file_name + '\t' + '0' + '\n')
                 f.flush()
@@ -27,5 +30,18 @@ def batch_filter():
     print('Done')
 
 
+def single_image_test():
+    zx = zxing.BarCodeReader()
+    file_path = 'Samples/2Code/2code.jpg'
+    zx_data = zx.decode(file_path)
+    if 'http://xfujian.189.cn' not in zx_data.raw:
+        image = cv2.imread(file_path)
+        image = cv2.fillPoly(image, np.array([zx_data.points], dtype=np.int32), (255, 255, 255))
+        file_path = file_path.replace('2code', '1code')
+        cv2.imwrite(file_path, image)
+        zx_data = zx.decode(file_path)
+    print('Done')
+
+
 if __name__ == '__main__':
-    batch_filter()
+    single_image_test()
