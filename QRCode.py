4074f67e6de339785d8b27cdf388e332dfc562ba
Ebenezer Scrooge <1641367382@qq.com>
2020-08-20 19:04:31 +0800

    Update QRCode.py

--- QRCode.py
+++ QRCode.py
@@ -7,8 +7,8 @@ from tqdm import tqdm
 
 
 def batch_filter():
-    path = 'D:/BaiduNetdiskDownload/202005/202005.v2'
-    # path = '/tmp/Photos'
+    # path = 'D:/BaiduNetdiskDownload/202005/202005.v2'
+    path = '/tmp/Photos'
     zx = zxing.BarCodeReader()
     img_list = os.listdir(path)
     img_list.sort()
@@ -31,15 +31,16 @@ def batch_filter():
 def single_image_test(zx, file_path):
     new_path = file_path
     zx_data = zx.decode(new_path)
+    image = cv2.imread(new_path)
     count = 0
     while zx_data is not None and len(zx_data.raw) > 0:
         if 'http://xfujian.189.cn' not in zx_data.raw:
-            image = cv2.imread(new_path)
             image = cv2.fillPoly(image, np.array([zx_data.points], dtype=np.int32), (255, 255, 255))
             new_path = new_path.replace('.jpg', str(count).join(['_', '.jpg']))
             cv2.imwrite(new_path, image)
             count += 1
             zx_data = zx.decode(new_path)
+            os.remove(new_path)
         else:
             return True
     return False
@@ -47,3 +48,6 @@ def single_image_test(zx, file_path):
 
 if __name__ == '__main__':
     batch_filter()
+    # zx = zxing.BarCodeReader()
+    # file_path = 'Samples/2code.jpg'
+    # single_image_test(zx, file_path)
