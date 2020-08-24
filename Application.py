24ce4d13f8cbe7a79dd375384f63a2622f199130
Ebenezer Scrooge <1641367382@qq.com>
2020-08-24 09:03:53 +0800

    Add Sample

--- Application.py
+++ Application.py
@@ -1,2 +1,31 @@
-import flask
+import cv2
+import zxing
+import base64
+import numpy as np
+from QRCode import test_on_array
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+zx = zxing.BarCodeReader()
+
+
+@app.route('/qrcode_decoder', methods=['POST'])
+def qrc_decode_flask():
+    if 'URL' in request.form:
+        file_path = request.form['URL']
+        img_array = cv2.imread(file_path)
+        result = test_on_array(zx, img_array)
+    elif 'Base64' in request.form:
+        b64_data = request.form['Base64']
+        img_data = base64.urlsafe_b64decode(b64_data)
+        np_array = np.frombuffer(img_data, dtype=np.uint8)
+        img_array = cv2.imdecode(np_array, cv2.COLOR_BGR2RGB)
+        result = test_on_array(zx, img_array)
+    else:
+        result = False
+    return jsonify({'Result': str(result)}), 201
+
+
+if __name__ == '__main__':
+    app.run(port='2029', debug=True)
--- QRCode.py
+++ QRCode.py
@@ -5,11 +5,12 @@ import zxing
 import numpy as np
 from tqdm import tqdm
 
+zx = zxing.BarCodeReader()
+
 
 def batch_filter():
     # path = 'D:/BaiduNetdiskDownload/202005/202005.v2'
     path = '/tmp/Photos'
-    zx = zxing.BarCodeReader()
     img_list = os.listdir(path)
     img_list.sort()
     with open('valid_file.txt', mode='a+', encoding='utf-8') as f:
@@ -18,7 +19,7 @@ def batch_filter():
         f.seek(checked_length)
         for file_name in tqdm(img_list[checked_length:]):
             file_path = os.path.join(path, file_name)
-            has_qr = single_image_test(zx, file_path, use_enhance=True)
+            has_qr = single_image_test(file_path, use_enhance=True)
             if has_qr:
                 f.write(file_name + '\t' + '1' + '\n')
                 f.flush()
@@ -28,7 +29,7 @@ def batch_filter():
     print('Done')
 
 
-def single_image_test(zx, file_path, use_enhance):
+def single_image_test(file_path, use_enhance):
     new_path = file_path
     image = cv2.imread(new_path)
     zx_data = zx.decode(new_path, try_harder=True)
@@ -42,16 +43,20 @@ def single_image_test(zx, file_path, use_enhance):
             zx_data = zx.decode(new_path, try_harder=True)
             os.remove(new_path)
         else:
-            return True
+            return zx_data.raw
     if count == 0 and use_enhance:
         image_e = cv2.convertScaleAbs(image, alpha=1.8, beta=-90)
         cv2.imwrite(new_path, image_e)
-        return single_image_test(zx, file_path, use_enhance=False)
+        return single_image_test(file_path, use_enhance=False)
     return False
+
+
+def test_on_array(img_array):
+    cv2.imwrite('QRC_Temp/Temp.jpg', img_array)
+    result = single_image_test('QRC_Temp/Temp.jpg', use_enhance=True)
+    os.remove('QRC_Temp/Temp.jpg')
+    return result
 
 
 if __name__ == '__main__':
     batch_filter()
-    # zx = zxing.BarCodeReader()
-    # file_path = 'Samples/temp.jpg'
-    # single_image_test(zx, file_path, use_enhance=True)
