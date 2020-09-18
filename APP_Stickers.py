b963b7a1eb9a951583e0f67cfab2e39aab4b8a51
Ebenezer Scrooge <1641367382@qq.com>
2020-09-18 15:50:14 +0800

    Backup

---
+++ APP_Stickers.py
@@ -0,0 +1,193 @@
+import requests
+from flask import Flask, request
+import os
+import cv2
+import json
+import time
+from QRCode import test_on_array
+import base64
+from skimage import io
+import numpy as np
+import logging
+
+# no_found = 'no such id'
+no_found = -1
+
+CEPH_code = {
+    'query': '/ceph-server/ceph/query/',
+    'upload': '/ceph-server/ceph/upload/',
+    'save': '/ceph-server/ceph/save/'
+}
+# CEPH_code = {
+#     'query': '/imr-ceph-server/ceph/query/',
+#     'upload': '/imr-ceph-server/ceph/upload/',
+#     'save': '/imr-ceph-server/ceph/save/'
+# }
+ATOM_code = {
+    'qr': '/imr-ai-service/atomic_functions/qrcode_decode',
+    'dl': '/imr-ai-service/atomic_functions/sticker_detect',
+}
+app = Flask(__name__)
+
+
+def make_dir(make_dir_path):
+    path = make_dir_path.strip()
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
+
+
+def file_request(function_string, req_id, save_path='QRC_Temp'):
+    # server_url = 'http://134.134.13.81:8788'
+    server_url = 'http://192.168.14.212:29999'
+    server_url += CEPH_code[function_string]
+    if function_string in ['query', 'save']:
+        server_url += req_id
+        response = requests.post(server_url)
+    elif function_string == 'upload':
+        assert type(req_id) is dict
+        assert 'file' in req_id
+        bucket_dict = {'bucketName': 'sticker'}
+        response = requests.post(server_url, data=bucket_dict, files=req_id)
+    else:
+        response = {}
+    print("Complete post")
+    response.raise_for_status()
+    result = eval(
+        response.content.decode('utf-8').replace('true', 'True').replace('false', 'False').replace('null', 'None')
+    )
+    if function_string == 'query':
+        if result['data'] is None:
+            return no_found
+        file_url = result['data']['server'] + '/' + result['data']['url']
+        r = requests.get(file_url)
+        with open(save_path + '/' + result['data']['fileName'], 'wb') as f:
+            f.write(r.content)
+        return result['data']['fileName']
+    elif function_string == 'save':
+        if result['msg'] == '成功':
+            return req_id
+        else:
+            return -1
+    elif function_string == 'upload':
+        return result['data']['cephId']
+    else:
+        return None
+
+
+def process_request(function_string, req_dict):
+    server_url = 'http://127.0.0.1:2029'
+    server_url += ATOM_code[function_string]
+    headers = {
+        "Content-Type": "application/json; charset=UTF-8"
+    }
+    if req_dict is not None:
+        json_dict = json.dumps(req_dict)
+        response = requests.post(server_url, data=json_dict, headers=headers)
+    else:
+        response = requests.post(server_url, headers=headers)
+    print("Complete post")
+    response.raise_for_status()
+    result = eval(response.content.decode('utf-8').replace('true', 'True'))
+    return result
+
+
+# log init start
+log_dir_name = "logs"
+log_file_name = 'logger-' + time.strftime('%Y-%m-%d', time.localtime(time.time())) + '.log'
+log_file_folder = os.path.join(os.getcwd(), log_dir_name)
+make_dir(log_file_folder)
+log_file_str = log_file_folder + os.sep + log_file_name
+log_level = logging.INFO
+handler = logging.FileHandler(log_file_str, encoding='UTF-8')
+# handler.setLevel(log_level)
+app.logger.setLevel(log_level)
+logging_format = logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)s - %(message)s')
+handler.setFormatter(logging_format)
+app.logger.addHandler(handler)
+
+
+@app.route('/test')
+def img_start():
+    return json.dumps({"system": 0}, ensure_ascii=False)
+
+
+@app.route('/imr-ai-service/moderm_stickers/check/<file_id>', methods=['POST'])
+def check(file_id):
+    log_file_name = 'logger-' + time.strftime('%Y-%m-%d', time.localtime(time.time())) + '.log'
+    log_file_str = log_file_folder + os.sep + log_file_name
+    if not os.path.exists(log_file_str):
+        handler = logging.FileHandler(log_file_str, encoding='UTF-8')
+        handler.setFormatter(logging_format)
+        app.logger.addHandler(handler)
+
+    if request.method == "POST":
+        file_id = file_id.replace("\n", "")
+        time_take = time.time()
+
+        file_name = file_request(function_string='query', req_id=file_id)
+        if file_name == no_found:
+            result = -1
+            nf = no_found
+        else:
+            nf = None
+            with open('QRC_Temp/' + file_name, 'rb') as f:
+                b64_string = base64.b64encode(f.read())
+                b64_string = b64_string.decode()
+                b64_string = 'data:image/jpeg;base64,' + b64_string
+            result = process_request('fd', req_dict={'imgString': b64_string})
+            if len(result['res']) > 0:
+                new_result = []
+                for rect in result['res']:
+                    img = cv2.imread('QRC_Temp/' + file_name)
+                    img = img[rect[1]:rect[3], rect[0]:rect[2]]
+                    cv2.imwrite('QRC_Temp/cropped_' + file_name, img)
+                    with open('QRC_Temp/cropped_' + file_name, 'rb') as fc:
+                        b64_string = base64.b64encode(fc.read())
+                        b64_string = b64_string.decode()
+                        b64_string = 'data:image/jpeg;base64,' + b64_string
+                    points = process_request('ld', req_dict={'imgString': b64_string})
+                    for point in points['res']:
+                        img = cv2.circle(img, tuple(point), 2, (255, 0, 0), 1)
+                    cv2.imwrite('QRC_Temp/cropped_' + file_name, img)
+                    uploaded_id = file_request(
+                        'upload',
+                        {
+                            'file': open(
+                                'QRC_Temp/cropped_' + file_name,
+                                'rb'
+                            )
+                        }
+                    )
+                    ret = file_request('save', uploaded_id)
+                    if ret == uploaded_id:
+                        new_result.append(uploaded_id)
+                os.remove('QRC_Temp/cropped_' + file_name)
+                new_result = ','.join(new_result)
+                result = new_result
+                print(new_result)
+            else:
+                result = -1
+            os.remove('QRC_Temp/' + file_name)
+        time_take = time.time() - time_take
+
+        ret = not (result == -1)
+        msg = {True: '成功', False: '失败'}
+        if nf is not None:
+            result = nf
+        return json.dumps(
+            {
+                'ret': ret,
+                'msg': msg[ret],
+                'data': result,
+                'timeTake': round(time_take, 4)
+            },
+            ensure_ascii=False
+        )
+
+if __name__ == '__main__':
+    app.run(
+        host="0.0.0.0",
+        port=int("1224"),
+        debug=False, threaded=True)
--- Atomic_Stickers.py
+++ Atomic_Stickers.py
@@ -1,15 +1,13 @@
-import base64
-import json
-import logging
-import os
-import time
-
-import cv2
-import numpy as np
 from flask import Flask, request
+import os
+import cv2
+import json
+import time
+from QRCode import test_on_array
+import base64
 from skimage import io
-
-from DeepFilter.test import test_on_array, load_weight
+import numpy as np
+import logging
 
 app = Flask(__name__)
 
@@ -42,8 +40,56 @@ def img_start():
     return json.dumps({"system": 0}, ensure_ascii=False)
 
 
-@app.route('/detect', methods=['POST'])
-def img_test():
+@app.route('/imr-ai-service/atomic_functions/qrcode_decode', methods=['POST'])
+def qr_decode():
+    log_file_name = 'logger-' + time.strftime('%Y-%m-%d', time.localtime(time.time())) + '.log'
+    log_file_str = log_file_folder + os.sep + log_file_name
+    if not os.path.exists(log_file_str):
+        handler = logging.FileHandler(log_file_str, encoding='UTF-8')
+        handler.setFormatter(logging_format)
+        app.logger.addHandler(handler)
+
+    if request.method == "POST":
+        start_time = time.time()
+        c_da = request.data
+        data = str(c_da, "utf-8")
+
+        data = eval(c_da.decode())
+        img_string = data['imgString'].encode()
+        img_string = img_string.decode()
+        # print(imgString)
+        img = np.array([])
+        if "base64," in str(img_string):
+            img_string = data['imgString'].encode().split(b';base64,')[-1]
+
+        if ".jpg" in str(img_string) or ".png" in str(img_string):
+            # print(imgString)
+            app.logger.info(data)
+            img_string = img_string.replace("\n", "")
+            img_rgb = io.imread(img_string)
+            img = cv2.cvtColor(np.array(img_rgb), cv2.COLOR_RGB2BGR)
+
+        if len(img_string) > 200:
+            img_string = base64.b64decode(img_string)
+            np_array = np.frombuffer(img_string, np.uint8)
+            img = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+
+        time_take = time.time()
+
+        result = test_on_array(img)
+
+        time_take = time.time() - time_take
+
+        if "fileName" in data.keys():
+            app.logger.info("recognition  return:{d},use time:{t}".format(d=result, t=time_take))
+            return json.dumps({data['fileName']: [{'value': result}]}, ensure_ascii=False)
+        # os.remove(path)
+        return json.dumps({'res': result, 'timeTake': round(time_take, 4)},
+                          ensure_ascii=False)
+
+
+@app.route('/imr-ai-service/atomic_functions/sticker_detect', methods=['POST'])
+def sticker_detect():
     log_file_name = 'logger-' + time.strftime('%Y-%m-%d', time.localtime(time.time())) + '.log'
     log_file_str = log_file_folder + os.sep + log_file_name
     if not os.path.exists(log_file_str):
@@ -99,7 +145,6 @@ def img_test():
 
 
 if __name__ == '__main__':
-    load_weight(file_path='Models/QRCode_Detector.h5')
     app.run(
         host="0.0.0.0",
         port=int("1224"),
