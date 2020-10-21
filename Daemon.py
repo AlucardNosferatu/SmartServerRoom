aa179d677ff240291af8c821340611b6b95fd9b6
Ebenezer Scrooge <1641367382@qq.com>
2020-10-21 17:29:20 +0800

    Update Daemon.py

--- Daemon.py
+++ Daemon.py
@@ -13,27 +13,20 @@ log_path = [AtD, ApF, AtF, ApM, AtM, ApP, AtP]
 
 if __name__ == '__main__':
     pid_list = ['0'] * len(log_path)
-    count = 0
-
     while True:
-        if count == 0:
-            for i in range(len(log_path)):
-                with open(log_path[i], 'r') as f:
-                    pid_list[i] = f.readlines()[0].strip()
+        for i in range(len(log_path)):
+            with open(log_path[i], 'r') as f:
+                pid_list[i] = f.readlines()[0].strip()
 
         process = os.popen('ps -ef | grep python')
-        current_pids = process.readlines()
+        current_pid_list = process.readlines()
 
         alive = [False] * len(log_path)
         for i, pid in enumerate(pid_list):
-            for current_pid in current_pids:
+            for current_pid in current_pid_list:
                 if pid in current_pid:
                     alive[i] = True
                     break
-
         print(alive)
-
-        count += 1
-        if count >= 20:
-            count = 0
-        time.sleep(5)
+        print(pid_list)
+        time.sleep(1)
