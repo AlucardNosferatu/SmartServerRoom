03b342a30a881c6186bf65c0b50d152a8fccc5e0
Ebenezer Scrooge <1641367382@qq.com>
2020-10-21 17:19:07 +0800

    Backup

--- Daemon.py
+++ Daemon.py
@@ -9,16 +9,31 @@ AtM = 'MotionDet/M_Temp/atomic_pid.txt'
 ApP = 'PedestrianDet/P_Temp/app_pid.txt'
 AtP = 'PedestrianDet/P_Temp/atomic_pid.txt'
 
+log_path = [AtD, ApF, AtF, ApM, AtM, ApP, AtP]
+
 if __name__ == '__main__':
-    AtD_pid = 0
-    ApF_pid = 0
-    AtF_pid = 0
-    ApM_pid = 0
-    AtM_pid = 0
-    ApP_pid = 0
-    AtP_pid = 0
+    pid_list = ['0'] * len(log_path)
+    count = 0
+
     while True:
-        with open(AtD, 'r') as f:
-            AtD_pid = int(f.readlines()[0].strip())
-        print('AtD_pid', AtD_pid)
+        if count == 0:
+            for i in range(len(log_path)):
+                with open(log_path[i], 'r') as f:
+                    pid_list[i] = f.readlines()[0].strip()
+
+        process = os.popen('ps -ef | grep python')
+        current_pids = process.readlines()
+
+        alive = [False] * len(log_path)
+        for i, pid in enumerate(pid_list):
+            for current_pid in current_pids:
+                if pid in current_pid:
+                    alive[i] = True
+                    break
+
+        print(alive)
+
+        count += 1
+        if count >= 20:
+            count = 0
         time.sleep(5)
