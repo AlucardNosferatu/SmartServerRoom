4d1019f2fdcf45a0e2371e3a87d294a91fe4ff4c
Ebenezer Scrooge <1641367382@qq.com>
2020-10-21 17:46:02 +0800

    Update Daemon.py

--- Daemon.py
+++ Daemon.py
@@ -10,24 +10,39 @@ AtM = 'MotionDet/M_Temp/atomic_pid.txt'
 ApP = 'PedestrianDet/P_Temp/app_pid.txt'
 AtP = 'PedestrianDet/P_Temp/atomic_pid.txt'
 
-log_path = [AtD, ApF, AtF, ApM, AtM, ApP, AtP]
+pid_path = [AtD, ApF, AtF, ApM, AtM, ApP, AtP]
+log_path = 'pid.txt'
 
 if __name__ == '__main__':
-    pid_list = ['0'] * len(log_path)
+    pid_list = ['0'] * len(pid_path)
     while True:
-        for i in range(len(log_path)):
-            with open(log_path[i], 'r') as f:
+        for i in range(len(pid_path)):
+            with open(pid_path[i], 'r') as f:
                 pid_list[i] = f.readlines()[0].strip()
-
         process = os.popen('ps -ef | grep python')
         current_pid_list = process.readlines()
-
-        alive = [False] * len(log_path)
+        alive = [False] * len(pid_path)
         for i, pid in enumerate(pid_list):
             for current_pid in current_pid_list:
                 if pid in current_pid:
                     alive[i] = True
                     break
-        print(str(datetime.datetime.now()), str(alive))
-        print(str(datetime.datetime.now()), str(pid_list))
+        info = [
+            ' '.join(
+                [
+                    str(datetime.datetime.now()),
+                    str(alive)
+                ]
+            )
+        ]
+        info += [
+            ' '.join(
+                [
+                    str(datetime.datetime.now()),
+                    str(pid_list)
+                ]
+            )
+        ]
+        with open(log_path, 'a') as f:
+            f.writelines(info)
         time.sleep(1)
