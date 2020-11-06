39a9be0e963489f166e8013f590a9f6eccbb546c
Ebenezer Scrooge <1641367382@qq.com>
2020-11-06 09:06:48 +0800

    Backup

--- CallbackListener/Atomic_Callback.py
+++ CallbackListener/Atomic_Callback.py
@@ -1,9 +1,28 @@
+import logging
 import os
 import time
 import json
 import datetime
 from flask import Flask, request
 
+work_dir = __file__
+if '\\' in work_dir:
+    work_dir = work_dir.split('\\')
+    work_dir = work_dir[:-1]
+    work_dir = '\\'.join(work_dir)
+else:
+    work_dir = work_dir.split('/')
+    work_dir = work_dir[:-1]
+    work_dir = '/'.join(work_dir)
+save_path = os.path.join(work_dir, 'C_Temp')
+log_path = os.path.join(save_path, 'CB.txt')
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename=log_path,
+    datefmt='%Y/%m/%d %H:%M:%S',
+    format='%(asctime)s - %(levelname)s - %(thread)d - %(module)s - %(funcName)s - %(lineno)d - %(message)s'
+)
+logger = logging.getLogger('CB')
 app = Flask(__name__)
 
 
@@ -13,17 +32,21 @@ def convert():
         time_take = time.time()
         c_da = request.data
         print('============================')
+        logger.info('============================')
         print(str(datetime.datetime.now()))
         try:
             data = json.loads(c_da.decode())
             for key in data:
                 print('键：', key, '值：', data[key])
+                logger.debug('键：' + str(key) + ' ' + '值：' + str(data[key]))
             ret = True
         except Exception as e:
             print(repr(e))
+            logger.error(repr(e))
             ret = False
         print(str(datetime.datetime.now()))
         print('============================')
+        logger.info('============================')
         time_take = time.time() - time_take
         msg = {True: '成功', False: '失败'}
         return json.dumps(
@@ -38,7 +61,7 @@ def convert():
 if __name__ == '__main__':
     pid = os.getpid()
     print('pid is:', pid)
-    with open('CBL_pid.txt', 'w') as f:
+    with open(os.path.join(save_path, 'atomic_pid.txt'), 'w') as f:
         f.writelines([str(pid)])
     app.run(
         host="0.0.0.0",
