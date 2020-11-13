829d3bd6c54d6206a5bff4abdbc4b1b390f93d7a
Ebenezer Scrooge <1641367382@qq.com>
2020-11-13 16:09:35 +0800

    Update GetTemp.py

--- GetTemp.py
+++ GetTemp.py
@@ -3,40 +3,48 @@ import numpy as np
 # import pytesseract
 import easyocr
 
-path = 'Samples/20201010_165228_channelT.dav'
-
 reader = easyocr.Reader(['ch_sim', 'en'])  # need to run only once to load model into memory
 
-cap = cv2.VideoCapture(path)
-old_frame = []
-diff = []
-count = 0
-while True:
-    if count % 1 == 0:
-        ret, frame = cap.read()
-        if ret:
-            frame = cv2.resize(frame, (1024, 768))
-            # cv2.imshow('full', frame)
-            temp_digit = frame[140:180, 880:970, :]
-            temp_digit = cv2.resize(temp_digit, (600, 250))
-            cv2.imshow('color', temp_digit)
-            temp_digit = cv2.convertScaleAbs(temp_digit, alpha=2, beta=-127)
-            cv2.imshow('gray', temp_digit)
-            colorLow = np.array([180, 180, 180])
-            colorHigh = np.array([255, 255, 255])
-            mask = cv2.inRange(temp_digit, colorLow, colorHigh)
-            temp_digit = cv2.cvtColor(cv2.bitwise_and(temp_digit, temp_digit, mask=mask), cv2.COLOR_RGB2GRAY)
-            cv2.imshow('mask-plain', temp_digit)
-            ret_th, temp_digit = cv2.threshold(temp_digit, 127, 255, cv2.THRESH_BINARY)
-            cv2.imshow('thresh', temp_digit)
-            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
-            temp_digit = cv2.morphologyEx(temp_digit, cv2.MORPH_CLOSE, kernel, iterations=5)
-            cv2.imshow('closed', temp_digit)
-            result = reader.recognize(temp_digit)
-            print(result[0][1])
-            cv2.waitKey(1)
+
+def resize_and_crop(frame):
+    frame = cv2.resize(frame, (1024, 768))
+    temp_digit = frame[140:180, 880:970, :]
+    temp_digit = cv2.resize(temp_digit, (600, 250))
+    return temp_digit
+
+
+def color_filter(frame):
+    temp_digit = cv2.convertScaleAbs(frame, alpha=2, beta=-127)
+    colorLow = np.array([180, 180, 180])
+    colorHigh = np.array([255, 255, 255])
+    mask = cv2.inRange(temp_digit, colorLow, colorHigh)
+    temp_digit = cv2.cvtColor(cv2.bitwise_and(temp_digit, temp_digit, mask=mask), cv2.COLOR_RGB2GRAY)
+    return temp_digit
+
+
+def img_enhance(frame):
+    frame = resize_and_crop(frame)
+    frame = color_filter(frame)
+    ret_th, temp_digit = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
+    temp_digit = cv2.morphologyEx(temp_digit, cv2.MORPH_CLOSE, kernel, iterations=5)
+    return temp_digit
+
+
+def process_video(path='Samples/20201010_165228_channelT.dav'):
+    cap = cv2.VideoCapture(path)
+    count = 0
+    while True:
+        if count % 1 == 0:
+            ret, frame = cap.read()
+            if ret:
+                temp_digit = img_enhance(frame)
+                cv2.imshow('closed', temp_digit)
+                result = reader.recognize(temp_digit)
+                print(result[0][1])
+                cv2.waitKey(1)
+            else:
+                break
         else:
-            break
-    else:
-        cap.grab()
-    count += 1
+            cap.grab()
+        count += 1
