f22acd7e8354e0130c455bd936e8d3aa9737ece0
Ebenezer Scrooge <1641367382@qq.com>
2020-12-12 22:03:16 +0800

    Update test_pyinjector.py

--- ConfInjector/test/test_pyinjector.py
+++ ConfInjector/test/test_pyinjector.py
@@ -1,11 +1,8 @@
-import sys
 import time
-from subprocess import Popen, PIPE
-from importlib.util import find_spec
+
 from pyinjector import inject
 
-INJECTION_LIB_PATH = find_spec('injection').origin
-STRING_PRINTED_FROM_LIB = b'Hello, world!'
+INJECTION_LIB_PATH = 'injection/build/lib.win-amd64-3.7/injection.cp37-win_amd64.pyd'
 TIME_TO_WIT_FOR_PROCESS_TO_INIT = 1
 TIME_TO_WIT_FOR_INJECTION_TO_RUN = 1
 
@@ -13,8 +10,7 @@ TIME_TO_WIT_FOR_INJECTION_TO_RUN = 1
 def test_inject():
     try:
         time.sleep(TIME_TO_WIT_FOR_PROCESS_TO_INIT)
-        pid_override = input()
-        inject(int(pid_override), INJECTION_LIB_PATH)
+        inject(int(input()), INJECTION_LIB_PATH)
         time.sleep(TIME_TO_WIT_FOR_INJECTION_TO_RUN)
     except Exception as e:
         print(repr(e))
