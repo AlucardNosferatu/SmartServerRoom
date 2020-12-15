47d373cf1ad2bae2654c20149718968e7cc26fd0
Ebenezer Scrooge <1641367382@qq.com>
2020-12-15 17:07:22 +0800

    Update GetCAPTCHA.py

--- Thermography/GetCAPTCHA.py
+++ Thermography/GetCAPTCHA.py
@@ -5,10 +5,20 @@ import easyocr
 
 reader = easyocr.Reader(['ch_sim', 'en'])  # need to run only once to load model into memory
 
+
+def enhance(img):
+    img = cv2.convertScaleAbs(img, alpha=2, beta=-127)
+    ret_th, img_th = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    c = 0.25
+    img = cv2.addWeighted(img_th, c, img, 1 - c, 0)
+    return img
+
+
 if __name__ == '__main__':
     path = '../Samples/OCR'
     for file in os.listdir(path):
         img = cv2.imread(os.path.join(path, file))
+        img = enhance(img)
         result = reader.recognize(img)
         result_str = result[0][1]
         new_str_list = []
