66f5ac500216ead1eb121eb7bb8296d712c9ebd5
Ebenezer Scrooge <1641367382@qq.com>
2020-12-17 17:16:57 +0800

    Delete UseImageAI.py

--- PedestrianDet/UseImageAI.py
+++
@@ -1,9 +0,0 @@
-from imageai.Detection import ObjectDetection
-import os
-
-exec_path = os.getcwd()
-detector = ObjectDetection()
-detector.setModelTypeAsRetinaNet()
-detector.setModelPath('Models/resnet50_coco_best_v2.0.1.h5')
-detector.loadModel()
-
