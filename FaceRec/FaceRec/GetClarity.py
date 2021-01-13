b13aaed0847845babb85a762fb99058c477c2f07
Ebenezer Scrooge <1641367382@qq.com>
2021-01-13 11:38:08 +0800

    Update PD

---
+++ FaceRec/FaceRec/GetClarity.py
@@ -0,0 +1,36 @@
+import os
+import cv2
+import numpy as np
+from Obsolete.HistUtil import calc_and_draw_hist
+
+
+def get_clarity():
+    path = "C:\\Users\\16413\\Downloads\\pic(1)"
+    for e, i in enumerate(os.listdir(path)):
+        if i.endswith(".PNG"):
+            img = cv2.imread(os.path.join(path, i))
+            hist_img, hist = calc_and_draw_hist(img, [255, 0, 255], None)
+            dark = np.sum(np.squeeze(hist)[:128])
+            bright = np.sum(np.squeeze(hist)[128:])
+            dbr = dark / bright
+            img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            image_var = cv2.Laplacian(img_gray, cv2.CV_64F).var()  # 图像模糊度
+            res = img.shape[0] * img.shape[1]
+            clarity = int(image_var) * 10000 / res
+            if dbr < 2 and clarity > 13:
+                print(i, " ", clarity, " ", dbr)
+            else:
+                print(i, "neg")
+
+
+def blurify():
+    path = "../Samples"
+    for e, i in enumerate(os.listdir(path)):
+        for size in [5, 11, 21]:
+            if i.endswith(".jpg"):
+                img = cv2.imread(os.path.join(path, i), 1)
+                blur = cv2.GaussianBlur(img, (size, size), 0)
+                cv2.imwrite(os.path.join(path, i).replace(".jpg", "_blur" + str(size) + ".jpg"), blur)
+
+
+get_clarity()
--- PedestrianDet/Atomic_Pedestrian.py
+++ PedestrianDet/Atomic_Pedestrian.py
@@ -1,52 +1,18 @@
 import json
-import logging
 import os
 import time
 from flask import Flask, request
 
 from cfg_PD import save_path
 from utils_PD import b64string2array
-from Haar import test_on_array
+# from Haar import test_on_array
+from UseImageAI import test_on_array
 
 app = Flask(__name__)
 
 
-def make_dir(make_dir_path):
-    path = make_dir_path.strip()
-    if not os.path.exists(path):
-        os.makedirs(path)
-    return path
-
-
-# log init start
-log_dir_name = "logs"
-log_file_name = 'logger-' + time.strftime('%Y-%m-%d', time.localtime(time.time())) + '.log'
-log_file_folder = os.path.join(os.getcwd(), log_dir_name)
-make_dir(log_file_folder)
-log_file_str = log_file_folder + os.sep + log_file_name
-log_level = logging.INFO
-handler = logging.FileHandler(log_file_str, encoding='UTF-8')
-# handler.setLevel(log_level)
-app.logger.setLevel(log_level)
-logging_format = logging.Formatter(
-    '%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)s - %(message)s')
-handler.setFormatter(logging_format)
-app.logger.addHandler(handler)
-
-
-@app.route('/test')
-def img_start():
-    return json.dumps({"system": 0}, ensure_ascii=False)
-
-
 @app.route('/imr-ai-service/atomic_functions/detect_pedestrian', methods=['POST'])
 def detect_p():
-    log_file_name = 'logger-' + time.strftime('%Y-%m-%d', time.localtime(time.time())) + '.log'
-    log_file_str = log_file_folder + os.sep + log_file_name
-    if not os.path.exists(log_file_str):
-        handler = logging.FileHandler(log_file_str, encoding='UTF-8')
-        handler.setFormatter(logging_format)
-        app.logger.addHandler(handler)
     if request.method == "POST":
         c_da = request.data
         data = json.loads(c_da.decode())
@@ -60,9 +26,6 @@ def detect_p():
             result = test_on_array(img)
             print('结束')
         time_take = time.time() - time_take
-        if "fileName" in data.keys():
-            app.logger.info("recognition  return:{d},use time:{t}".format(d=result, t=time_take))
-            return json.dumps({data['fileName']: [{'value': result}]}, ensure_ascii=False)
         # os.remove(path)
         return json.dumps({'res': result, 'timeTake': round(time_take, 4)},
                           ensure_ascii=False)
